a7cecc47a5cd2da191dcc1950deeac729ee93eba
Daniel Miranda <dmg1428@gmail.com>
2025-10-29 23:25:21 -0300

    Commit Inicial

---
+++ soundwave/src/components/ProductCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ProductCard({ img, name, desc, price, id }) {
+  return (
+    <div className="produto" id={id}>
+      <img src={img} alt={name} />
+      <h3>{name}</h3>
+      <p>{desc}</p>
+      <span>{price}</span>
+    </div>
+  );
+}
+
+export default ProductCard;
